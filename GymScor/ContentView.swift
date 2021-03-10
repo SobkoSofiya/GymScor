9eb078abe51b98f28118761c114afafe67e3eda4
SobkoSofiya <sobkosofiya23@gmail.com>
2021-03-10 10:08:34 +0300

    Initial Commit

---
+++ GymScor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GymScor
+//
+//  Created by Sofi on 10.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
